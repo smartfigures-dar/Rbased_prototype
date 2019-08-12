e3c2451fc14701947d3dbd09ac11e7b88122cdb0
colombj <julien.colomb@fu-berlin.de>
2019-08-12 23:40:56 +0200

    add project entry, extension

--- figureimport.R
+++ figureimport.R
@@ -19,6 +19,8 @@ if (FALSE){
   highlight = "FALSE"
   author = "jco"
   lab="lkm"
+  thisproject = "project_not_described"
+  updated_by = "julien colomb"
   
 }
 dropboxmessage = "Beware files were not saved on dropbox, token not uploaded."
@@ -79,6 +81,78 @@ headers$Highlighted [1] <- highlight
 write.table(headers,file = paste0(directory,"/",filename,"_meta.tsv"), 
             sep = "\t", , row.names = FALSE)
 
+projects_authors <- read_csv("./static/ResultGallery/projects_authors.csv")
+projects_authors= projects_authors %>% filter (project == thisproject) %>% select(- Project_title)
+sink (file = paste0(directory,"/",filename,".yml"), append = FALSE)
+
+cat (paste0("# data uplaoded by ",updated_by, " inside the project ", thisproject))
+
+cat ("authors:
+")
+
+for (j in c(1:nrow(projects_authors))) {
+  
+  cat(
+'  -
+    firstname: "', projects_authors$contributor_firstname[j], '"
+    lastname: "', projects_authors$contributor_lastname[j],'"
+    affiliation: "',projects_authors$contributor_affiliation[j], '"'
+, sep = "")
+  
+  if (!is.na(projects_authors$contributor_orcid[j])) {
+    cat('
+    id: "http://orcid.org/', projects_authors$contributor_orcid[j], '"'
+      , sep = "")    
+  }
+}
+
+cat ('
+title: "',title1,'"' , sep = "")
+
+cat ('
+description: |
+  ',caption, sep = "")
+
+cat ('
+license:
+  name: "CC-BY"
+  url: "https://creativecommons.org/licenses/by/4.0/"
+
+## Optional Fields
+
+# Any funding reference for this resource. Separate funder name and grant number by comma
+funding:
+  - "DFG, Project number 327654276 – SFB 1315"', sep ="")
+if (url != "none"){
+  cat('
+# Related publications. reftype might be: IsCitedBy, IsSupplementTo, IsReferencedBy, IsPartOf
+# for further valid types see https://schema.datacite.org/meta/kernel-4
+# Please provide digital identifier (e.g., DOI) if possible.
+references:
+  -
+    doi: "', url,'"
+    reftype: "IsPartOf"
+    name: "noName"    
+    
+    ')}
+  
+
+
+
+#cat ("
+#     ")
+#kable (projects_authors %>% filter (project == thisproject) %>% select(- Project_title))
+
+sink()
+
+authors:
+  -
+  firstname: "GivenName1"
+lastname: "FamilyName1"
+affiliation: "Affiliation1"
+id: "AuthorID1 (e.g. ORCID)"
+
+
 ##------------------------------------ create figures
 
 #library (magick)
@@ -122,6 +196,7 @@ if(exists("tokenRG")){
   drop_upload(file =paste0(directory,"/",filename,"_meta.tsv"),path =paste0(dropboxfolder) , dtoken = tokenRG)
   drop_upload(file =paste0(directory,"/",filename,"_nail.png"),path =paste0(dropboxfolder), dtoken = tokenRG)
   drop_upload(file =paste0(directory,"/",filename,".png"),path =paste0(dropboxfolder), dtoken = tokenRG)
+  drop_upload(file =paste0(directory,"/",filename,".yml"),path =paste0(dropboxfolder), dtoken = tokenRG)
   
   metadata= headers
   filepath = paste0(directory,"/",filename,"exp.pdf")
